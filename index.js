fb10733596776c68ae08719a9a20b486585361bf
Sarvesh Biradar <sbiradar@pdx.edu>
2023-06-07 17:00:20 -0700

    Made changes according to feedbacks given in HW.
    1. only one h1 tag
    2. form eventlistener instead of onsubmit.
    3. improved meta tags.
    4. responsive style changes.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const formSubmit = () => {
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const date = document.getElementById("message").value;
-  if (!name || !email || !date) {
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  if (!name || !email || !message) {
     alert("Please fill all fields");
   } else {
     alert("This doesn't work yet. Why not connect on LinkedIn?");
-    document.getElementById("form").reset();
+    form.reset();
   }
-};
+});
